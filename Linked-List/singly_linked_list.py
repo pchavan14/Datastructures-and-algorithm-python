e2cce86fa7c3e42838beccc7e9dc28c548f81b75
Prachi Chavan <prachichavan@Prachis-MacBook-Pro.local>
2023-04-04 15:48:56 -0700

    tuples and singly linked list

---
+++ Linked-List/singly_linked_list.py
@@ -0,0 +1,131 @@
+#operations on singly linked list
+
+#1. create head and tail and assign NULL reference to it
+#2. Create node with data and NULL reference
+#3. Point head and tail to that node
+
+#Node class
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+#SSL class
+class singly_linked_list:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            node = node.next
+
+
+    def insertSLL(self,value,location):
+        newNode = Node(value)
+
+        #if the SLL is empty , reference head and tail to this node
+        if self.head is None:
+            self.head = newNode
+            self.tail = newNode
+        else:
+             #beginning of sll
+            if location == 0: #Time complexity O(1)
+                newNode.next = self.head #head stores first node physical location
+                self.head = newNode
+            #insert node at the end of SLL
+            elif location == -1: #Time complexity O(1)
+                newNode.next = None
+                self.tail.next = newNode
+                self.tail = newNode
+            #insert node between two nodes
+            else:
+                tempNode =  self.head
+                index = 0
+                while (index < location -1): #Time complexity O(n)
+                    tempNode = tempNode.next
+                    index += 1
+                nextNode = tempNode.next
+                tempNode.next = newNode
+                newNode.next = nextNode
+                if tempNode == self.tail:
+                    self.tail = newNode
+
+    def traverseSSL(self):
+        if self.head is None:
+            print("The singly linked list does not exist")
+        else:
+            temp = self.head
+            while temp is not None:  #Time complexity - O(n)
+                print(temp.value)
+                temp = temp.next
+
+    def searchSSL(self,value):
+        if self.head is None:
+            return ("the list is empty")
+        else:
+            temp = self.head
+            while temp is not None: #Time complexity - O(n)
+                if temp.value == value:
+                    return("the element exits in the linked list")
+                temp = temp.next
+            return ("The element does not exist in the linked list")
+        
+    #delete a node from the linked list
+    def deletenodeSSL(self,location):
+        if self.head is None:
+            return ("The SSL does not exist")
+        else:
+            #delete the first node
+            if location == 0:
+                #check if it is the only node
+                if self.head == self.tail:
+                    self.head = None
+                    self.tail = None
+                else:
+                    self.head = self.head.next
+            #delete last node
+            elif location == -1:
+                if self.head == self.tail:
+                    self.head = None
+                    self.tail = None
+                else:
+                    temp = self.head
+                    while temp.next.next is not None:
+                        temp = temp.next
+                    temp.next = None
+                    self.tail = temp
+            #delete a node in between
+            else:
+                temp = self.head
+                index = 0
+                while index < location - 1:
+                    temp = temp.next
+                    index += 1
+                nextnode = temp.next
+                temp.next = nextnode.next
+                nextnode.next = None
+
+                
+
+                
+                
+                 
+
+
+
+
+#create linked list and nodes
+singly_ll = singly_linked_list()
+singly_ll.insertSLL(1,0)
+singly_ll.insertSLL(2,-1)
+singly_ll.insertSLL(4,-1)
+singly_ll.insertSLL(5,-1)
+singly_ll.insertSLL(3,2)
+singly_ll.traverseSSL()
+singly_ll.deletenodeSSL(3)
+singly_ll.traverseSSL()
+
+
+
---
+++ Tuples/tuples.py
@@ -0,0 +1,19 @@
+new_tuple = ('a','b','c','d')
+#time complexity = O(1)
+
+#with one element (include the comma)
+one_tuple = ('a',)
+
+#built in tuples
+tuple_new = tuple()
+
+#access elements of tuple like lists and also slice operator
+new_tuple[1]
+
+#traverse and search element is same as list
+
+#list to tuple
+a = tuple([1,2,3,4])
+print(a)
+
+
