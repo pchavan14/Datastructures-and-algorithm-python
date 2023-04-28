736fddba13b7e6817f95ffe652dacae3d61556ef
Prachi <prchavan1412@gmail.com>
2023-04-28 16:16:34 -0700

    LC grind

---
+++ Leetcode/medium/graph-number_of_operations_to_make_network_connected.py
@@ -0,0 +1,56 @@
+from collections import defaultdict
+class Solution(object):
+    def makeConnected(self, n, connections):
+        """
+        :type n: int
+        :type connections: List[List[int]]
+        :rtype: int
+        """
+
+        
+        if len(connections) < n-1:
+            return (-1)
+
+        adjacency_list = defaultdict(list)
+        visited = []
+        number_of_islands = 0
+        
+        for i in connections:
+            adjacency_list[i[0]].append(i[1])
+            adjacency_list[i[1]].append(i[0])
+        
+        
+        for node in range(n):
+            if node not in visited:
+                self.bfs(adjacency_list,node,visited)             
+                number_of_islands += 1
+    
+
+        return (number_of_islands - 1)
+
+
+
+    def bfs(self,adjacency_list,node,visited):
+        visited.append(node)
+        custom_queue = []
+        custom_queue.append(node)
+
+
+        while custom_queue:
+            deVertex = custom_queue.pop(0)
+            for edge in adjacency_list[deVertex]:
+                if edge not in visited:
+                    visited.append(edge)
+                    custom_queue.append(edge)
+        
+
+
+
+
+
+
+n = 6
+connections =  [[0,1],[0,2],[0,3],[1,2]]
+sol = Solution()
+sol.makeConnected(n,connections)
+
